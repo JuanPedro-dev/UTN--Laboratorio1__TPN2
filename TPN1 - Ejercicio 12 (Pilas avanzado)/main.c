be81a680617fb8f8a7ed07d1d47543e5c6e68f06
Juan Pedro <89544550+JuanPedro-dev@users.noreply.github.com>
2022-04-27 14:03:15 -0300

    Update main.c

--- TPN1 - Ejercicio 12 (Pilas avanzado)/main.c
+++ TPN1 - Ejercicio 12 (Pilas avanzado)/main.c
@@ -2,32 +2,28 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <locale.h>
+#include "../Librerias/pila.h"
 
 int main()
 {
     setlocale(LC_ALL,"");
     system("color 1E");
-    /** 12.Dada la pila ORIGEN ordenarla en forma ascendente por método de inserción dejando el resultado
+    /** 12.Dada la pila ORIGEN ordenarla en forma ascendente por mÃ©todo de inserciÃ³n dejando el resultado
         en la pila ORIGEN. Para este ejercicio usar lo aprendido para el ejercicio 5.
-        Método de inserción: ver cómo funciona
+        MÃ©todo de inserciÃ³n: ver cÃ³mo funciona
         */
-    /**
-    pila origen
-    Pila final ascendente osea  1 2 3 4 7
 
-    pila aux 7 4 3 2 1
-
-   7 4     3 2 1
-
-    */
-    Pila origen;
+    Pila origen, destino, auxiliar;
     inicpila(&origen);
-
-    Pila destino;
     inicpila(&destino);
-
-    Pila auxiliar;
     inicpila(&auxiliar);
+
+    // Pila de prueba:
+    apilar(&origen, 2);
+    apilar(&origen, 21);
+    apilar(&origen, 34);
+    apilar(&origen, 8);
+    apilar(&origen, 7);
 
     if(!pilavacia(&origen))
     {
@@ -36,21 +32,44 @@ int main()
 
         while(!pilavacia(&origen))
         {
-           // Recorro la pila aux y voy poniendo en el orden que va
-            while(!pilavacia(&destino))
+            // Recorro la pila origen y voy poniendo en el orden que va
+            while(!pilavacia(&origen))
             {
-                if(tope(&origen) < tope(&destino))
+                if(tope(&origen) > tope(&destino))
                 {
-
+                    apilar(&auxiliar, desapilar(&destino));
+                    apilar(&destino, desapilar(&origen));
+                }
+                else
+                {
+                    apilar(&auxiliar, desapilar(&origen));
                 }
             }
+            // rearmo origen
+            while(!pilavacia(&auxiliar))
+            {
+                apilar (&origen, desapilar(&auxiliar));
+            }
+            //Paso el tope para comparar si es el "nuevo menor"
+            if(!pilavacia(&origen))
+            {
+                apilar(&destino, desapilar(&origen));
+            }
         }
-    } else
+    }
+    else
     {
         printf("\nLa pila se encuentra vacia.\n");
     }
+    while(!pilavacia(&destino))
+    {
+        apilar(&origen,desapilar(&destino));
+    }
 
-    printf("                           Fin Programa!!!\n");
+    printf(" >>> La Pila origen ha sido ordenada: \n\n ");
+    mostrar(&origen);
+
+    printf("\n\n                           Fin Programa!!!\n");
     system("pause");
     return 0;
 }
